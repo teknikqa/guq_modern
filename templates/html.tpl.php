6101d420c895d4e6ad6e841a8e088eb39c17f446
Nick Mathew <nim8@georgetown.edu>
2016-10-06 15:09:44 +0300

    Rearrange order of tags in theme

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
   <head profile="<?php print $grddl_profile; ?>">
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-552cff9377870122"></script>
-    <script type="text/javascript">addthis.toolbox('.addthis_toolbox');</script>
-      <?php print $head; ?>
     <title><?php print $head_title; ?></title>
+    <?php print $head; ?>
     <meta http-equiv="cleartype" content="on">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <!-- typekit -->
-    <script type="text/javascript" src="//use.typekit.net/zat8mnd.js"></script>
-    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-    <!-- Fonts -->
-    <script type="text/javascript" src="//fast.fonts.net/jsapi/36b3fb32-8417-420e-90b7-8ec95a4e9de6.js"></script>
     <?php
     /*
     <!-- Icons ================ -->
@@ -27,12 +20,14 @@
     */
     global $base_url;
     ?>
-    <?php print $styles; ?>
+    <!-- Fonts -->
+    <script type="text/javascript" src="//fast.fonts.net/jsapi/36b3fb32-8417-420e-90b7-8ec95a4e9de6.js"></script>
     <?php print $scripts; ?>
     <!--[if lt IE 9]>
       <script src="<?php print $base_url; ?>/profiles/varuni/themes/guq_modern/js/html5-respond.js"></script>
       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+    <?php print $styles; ?>
   </head>
   <body class="<?php print $classes; ?> index" <?php print $attributes;?>>
     <div id="skip-links">
@@ -43,5 +38,11 @@
     <?php print $page_top; ?>
     <?php print $page; ?>
     <?php print $page_bottom; ?>
+    <!-- TypeKit -->
+    <script type="text/javascript" src="//use.typekit.net/zat8mnd.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <!-- AddThis -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-552cff9377870122"></script>
+    <script type="text/javascript">addthis.toolbox('.addthis_toolbox');</script>
   </body>
 </html>
